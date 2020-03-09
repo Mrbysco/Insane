9ee77dae5f0484b315215b59da70e784b380f18f
Mrbysco <Mrbysco@users.noreply.github.com>
2020-03-09 22:44:13 +0100

    Packet work

--- src/main/java/com/mrbysco/insane/Insane.java
+++ src/main/java/com/mrbysco/insane/Insane.java
@@ -4,6 +4,7 @@ import com.mrbysco.insane.capability.ISanity;
 import com.mrbysco.insane.capability.SanityCapability;
 import com.mrbysco.insane.capability.SanityStorage;
 import com.mrbysco.insane.handler.CapabilityHandler;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.capabilities.CapabilityManager;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -11,6 +12,8 @@ import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.network.NetworkRegistry;
+import net.minecraftforge.fml.network.simple.SimpleChannel;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -18,6 +21,14 @@ import org.apache.logging.log4j.Logger;
 public class Insane
 {
     public static final Logger LOGGER = LogManager.getLogger(Reference.MOD_ID);
+
+    private static final String PROTOCOL_VERSION = "1";
+    public static final SimpleChannel CHANNEL = NetworkRegistry.newSimpleChannel(
+            new ResourceLocation(Reference.MOD_ID, "main"),
+            () -> PROTOCOL_VERSION,
+            PROTOCOL_VERSION::equals,
+            PROTOCOL_VERSION::equals
+    );
 
     public Insane() {
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
--- src/main/java/com/mrbysco/insane/packets/SanitySyncMessage.java
+++ src/main/java/com/mrbysco/insane/packets/SanitySyncMessage.java
@@ -1,12 +1,10 @@
 package com.mrbysco.insane.packets;
 
-import com.mrbysco.insane.capability.ISanity;
 import com.mrbysco.insane.capability.SanityCapProvider;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.network.PacketBuffer;
-import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.fml.network.NetworkEvent;
 import net.minecraftforge.fml.network.NetworkEvent.Context;
 
@@ -33,9 +31,8 @@ public class SanitySyncMessage {
             if (ctx.getDirection().getReceptionSide().isClient() && ctx.getDirection().getOriginationSide().isServer()) {
                 PlayerEntity player = Minecraft.getInstance().player;
                 player.getCapability(SanityCapProvider.SANITY_CAPABILITY, null)
-                        .ifPresent(state -> {
-                            Capability.IStorage<ISanity> storage = SanityCapProvider.SANITY_CAPABILITY.getStorage();
-                            storage.readNBT(SanityCapProvider.SANITY_CAPABILITY, state, null, data);
+                        .ifPresent(sanityCap -> {
+                            SanityCapProvider.SANITY_CAPABILITY.readNBT(sanityCap, null, data);
                         });
             }
         });
